a9494991c82926f2571144d7442eb7089da3a3b7
JckD <c16335383@mydit.ie>
2018-12-03 13:05:53 +0000

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //connect to database
-    $con = mysqli_connect("localhost","root","","dt211");
-    
-    //check connection
-    if(mysqli_connect_errno()){
-        echo "Failed to connect to MYSQL: ". mysqli_connect_errno();
-    }
-?>
